f075141309bc5976883738f097f69a019c572117
Yoko-9707 <satomi0707apple@gmail.com>
2020-11-13 17:19:58 +0900

    既読機能のサーバーサイド実装　その３

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'posts#index' # get 'posts', to: 'posts#index'から変更
   # get 'posts/new', to: 'posts#new'を削除
   post 'posts', to: 'posts#create'
+  get 'posts/:id', to: 'posts#checked'
 end
