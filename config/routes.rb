d7af38148036007adc1c074c6105268c7d748595
Yoko-9707 <satomi0707apple@gmail.com>
2020-11-13 16:59:30 +0900

    Revert "既読機能のサーバーサイド実装"
    This reverts commit d6c8f0d112a81e9e05ae32ff4d7bc3a6bf47a39e.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   root to: 'posts#index' # get 'posts', to: 'posts#index'から変更
   # get 'posts/new', to: 'posts#new'を削除
   post 'posts', to: 'posts#create'
-  get 'post/:id', to: 'posts#checked'
 end
